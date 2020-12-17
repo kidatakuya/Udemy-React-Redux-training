5dae0cebf3e0a3386884d5985e1c41c27fa09e5f
kidatakuya <kida.takuya.1234@gmail.com>
2020-12-18 00:45:29 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,66 +1,32 @@
 import { render } from '@testing-library/react';
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-// import logo from './logo.svg';
-// import './App.css';
-
-// class App extends Component {
-//   render(){
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={()=>{console.log("I am clicked!")}} />
-//       </React.Fragment>
-//     );
-//   }
-  
-// }
 
 
-// reactにはバーチャルドムが存在し、どのドムが変更されたか、管理していて、変更点だけを実際のドムに反映している。
+const App = ( )=> ( <Counter></Counter> )
+	
 
-const App = ( )=>{
-	const profiles = [
-		{name: "Taro", age: 10},
-		{name: "Hanako", age: 5},
-		{name: "NoName",age: 4}
-	]
-	return <div> 
-				{
-					profiles.map((profile, index) => {
-						return <User name={profile.name} age={profile.age} key={index} />
-					})
-				}
-				
-				<User name={"Taro"} age={10} />
-				<User name={"Hanako"} age={5} />
-					
-			</div>
+
+// 
+// state
+// 
+
+// stateを使う時、前にthisをつける
+
+// classコンポーネント
+class Counter extends Component{
+
+	// constructorは初期化処理で実行されるメソッド
+	constructor(props){
+		super(props)
+		console.log(this.state)
+		this.state = { count: 0}
+	}
+	render(){
+		console.log(this.state)
+		return(<div>count: { this.state.count }</div>)
+	}
 }
 
-const User = ( props )=>{
-return <div> Hi, I am {props.name} and {props.age}years old!</div>
-}
-
-// propTypesはpropsで渡された値の肩チェックをするもの
-
-User.propTypes = {
-	name: PropTypes.string,
-	age: PropTypes.number.isRequired
-}
-
-// isRequiredはデータが入っているかの型チェックをする。
-// つまり、age に　 PropTypes.number.isRequired　数字のデータが入っていないとエラーが出ると言う事
-
-
-
-// defaultProps はpropsが空の時に入る値。
-
-// 構文
-// コンポーネント.defaultProps = {
-// 	name: 値,
-// 	name: 値,
-// }
 
 
 
