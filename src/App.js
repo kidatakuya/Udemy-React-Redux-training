ee6e09d75fa539ccd0a3ff7a31f2673847d47472
kidatakuya <kida.takuya.1234@gmail.com>
2020-12-22 23:09:35 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -21,9 +21,28 @@ class Counter extends Component{
 		console.log(this.state)
 		this.state = { count: 0}
 	}
+	handlePlusButton = () => {//handlePlusButtonと言う自作関数
+		this.setState({ count: this.state.count + 1})
+		// stateの値を帰る時、setStateを使って変更する
+
+		// this.state = { count: 0}
+		// state の値を変更するとき、上記のような初期設定時の記述方法での変更はしてはいけない。変更できない。
+		// setStateを使わないとDOMが再レンダーされない。
+	}
+	handleMinusButton = () => {//handlePlusButtonと言う自作関数
+		this.setState({ count: this.state.count - 1})
+		
+	}
+
 	render(){
 		console.log(this.state)
-		return(<div>count: { this.state.count }</div>)
+		return(
+			<React.Fragment>
+				<div>count: { this.state.count }</div>
+				<button onClick={this.handlePlusButton}>+1</button>
+				<button onClick={this.handleMinusButton}>-1</button>
+			</React.Fragment>
+		)
 	}
 }
 
