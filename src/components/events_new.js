458e1c55b249456aac6d1f21ec948fd329e45b46
kidatakuya <kida.takuya.1234@gmail.com>
2021-02-10 16:39:00 +0900

    update

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -26,7 +26,9 @@ class EventsNew extends Component{
 		this.props.history.push('/')
 	}
 	render(){
-		const { handleSubmit } = this.props
+		// pristine はテキストが入力されていないと送信できない。これの判定ができる
+		const { handleSubmit, pristine,submitting } = this.props
+		console.log(submitting)
 		return(
 			<form onSubmit={handleSubmit(this.onSubmit)}>
 				<div>
@@ -36,7 +38,7 @@ class EventsNew extends Component{
 					<Field label="Body" name="body" type="text" component={this.renderField} />
 				</div>
 				<div>
-					<input type="submit" value="Submit" disabled={false} />
+					<input type="submit" value="Submit" disabled={pristine || submitting} />
 					<Link to="/">Cancel</Link>
 				</div>
 			</form>
