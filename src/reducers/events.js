03d96b9435124769ddf083d7b6ad621ed75de4d7
kidatakuya <kida.takuya.1234@gmail.com>
2021-02-13 22:23:06 +0900

    implement eventsShow

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,15 +1,25 @@
 import _ from 'lodash'//axoisで帰ってきたデータを整理するパッケージ
-import { READ_EVENTS, DELETE_EVENTS } from '../actions'
+import { 
+    CREATE_EVENTS,
+    READ_EVENTS,
+    READ_EVENT,
+    DELETE_EVENTS,
+    UPDATE_EVENTS 
+} from '../actions'
 
 const initialState = { value: 0}
 
 export default ( events = {}, action) => {
     switch (action.type) {
+        case CREATE_EVENTS:
+        case READ_EVENT:
+        case UPDATE_EVENTS:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
         case READ_EVENTS:
             // console.log(action.response.data)
         return _.mapKeys(action.response.data,'id')//帰ってきたidを使って整理してくれる
         case DELETE_EVENTS:
-            console.log(action.id)
             delete events[action.id]
             return { ...events }
         default:
